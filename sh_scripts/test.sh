75e5a45e8cb1c08dfd2e16759a0d99e619edf2a1
kstasi <nastasiakondaurova@gmail.com>
2020-03-15 20:53:38 +0200

    pay to oracles tested

--- sh_scripts/test.sh
+++ sh_scripts/test.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 ./sh_scripts/02_create_register.sh
-fift -s tests/00_basic_register_tests.fif
+# fift -s tests/00_basic_register_tests.fif
 
-fift -s fift_scripts/register-oracle.fif
-fift -s tests/01_register_oracle_tests.fif
+# fift -s fift_scripts/register-oracle.fif
+# fift -s tests/01_register_oracle_tests.fif
 
-fift -s fift_scripts/register-provider.fif "some.com" "50" "0" "./build/new-public-oracle"
-fift -s tests/02_register_provider_tests.fif
+# fift -s fift_scripts/register-provider.fif "some.com" "50" "0" "./build/new-public-oracle"
+# fift -s tests/02_register_provider_tests.fif
 
-fift -s fift_scripts/send-int-data.fif "some.com" "50" "0" 
-fift -s tests/03_send_int_data_tests.fif
+# fift -s fift_scripts/send-int-data.fif "some.com" "50" "0" 
+# fift -s tests/03_send_int_data_tests.fif
 
-fift -s fift_scripts/request-data.fif "some.com" "./build/new-wallet" 
-fift -s tests/04_request_data_tests.fif
+# fift -s fift_scripts/request-data.fif "some.com" "./build/new-wallet" 
+# fift -s tests/04_request_data_tests.fif
+
+fift -s tests/05_pay_tests.fif
