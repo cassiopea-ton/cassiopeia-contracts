f54555bc602df095d99d86dcfceb9a8bfd75e122
kstasi <nastasiakondaurova@gmail.com>
2020-03-15 16:13:34 +0200

    register provider fixes

--- sh_scripts/test.sh
+++ sh_scripts/test.sh
@@ -4,3 +4,6 @@ fift -s tests/00_basic_register_tests.fif
 
 fift -s fift_scripts/register-oracle.fif
 fift -s tests/01_register_oracle_tests.fif
+
+fift -s fift_scripts/register-provider.fif "some.com" "50" "0" "./build/new-public-oracle"
+fift -s tests/02_register_provider_tests.fif
